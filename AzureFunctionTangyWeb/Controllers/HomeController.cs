aa52a3d18449bf708d07d67ece6d39d1d8241408
raisahmad6 <raisahmad1982@gmail.com>
2026-07-26 20:08:50 +0530

    added more files

---
+++ AzureFunctionTangyWeb/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using AzureFunctionTangyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace AzureFunctionTangyWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly BlobServiceClient _blobServiceClient;
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory, BlobServiceClient blobServiceClient)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+            _blobServiceClient = blobServiceClient;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //http://localhost:7183/api/OnSalesUploadWriteToQueue
+
+        [HttpPost]
+        public async Task<IActionResult> Index(SalesRequest salesRequest, IFormFile file)
+        {
+            salesRequest.Id = Guid.NewGuid().ToString();
+            using var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri("https://tangyazurefunction.azurewebsites.net/api/");
+            using (var content = new StringContent(JsonConvert.SerializeObject(salesRequest), System.Text.Encoding.UTF8, "application/json"))
+            {
+                HttpResponseMessage response = await client.PostAsync("OnSalesUploadWriteToQueue",content);
+                string returnValue = await response.Content.ReadAsStringAsync();
+
+            }
+
+            if (file != null)
+            {
+                var fileName = salesRequest.Id + Path.GetExtension(file.FileName);
+                BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient("functionsalesrep");
+                var blobClient = containerClient.GetBlobClient(fileName);
+
+                var httpheaders = new BlobHttpHeaders()
+                {
+                    ContentType = file.ContentType
+                };
+                await blobClient.UploadAsync(file.OpenReadStream(), httpheaders);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
